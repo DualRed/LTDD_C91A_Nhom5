9f733737994831019b75ececc7cc040fb8fd4e81
Dr.Red <baolinhkg1999@gmail.com>
2021-09-17 20:39:54 +0700

    Initial Commit

--- app/src/main/java/com/example/chatbot_app/MainActivity.java
+++ app/src/main/java/com/example/chatbot_app/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.chatbot_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton gitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gitBtn = findViewById(R.id.gitButton);
+
+        gitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/DualRed/chatbot-app");
+            }
+        });
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
